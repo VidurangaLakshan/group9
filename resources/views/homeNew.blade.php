d618fd9f816d476a857e8aab217d2c5ed5af7bcc
Viduranga Lakshan <>
2024-04-24 06:10:07 +0530

    Update homeNew.blade.php

--- resources/views/homeNew.blade.php
+++ resources/views/homeNew.blade.php
@@ -32,149 +32,6 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="slider-activation axil-slick-arrow">
-                                <!-- Start Single Slide  -->
-                                <div class="content-block">
-                                    <!-- Start Post Thumbnail  -->
-                                    <div class="post-thumbnail">
-                                        <a href="">
-                                            <img src="assets/images/post-images/gallery-post-01.jpg"
-                                                 alt="Post Images">
-                                        </a>
-                                    </div>
-                                    <!-- End Post Thumbnail  -->
-
-                                    <!-- Start Post Content  -->
-                                    <div class="post-content">
-                                        <div class="post-cat-list">
-                                            <a class="hover-flip-item-wrapper" href="#">
-                                                <span class="hover-flip-item">
-                                                    <span data-text="Explore IT Courses">Explore IT Courses</span>
-                                                </span>
-                                            </a>
-                                        </div>
-                                        <h2 class="title"><a href="">APIIT:
-                                                Future-ready education for impactful careers</a></h2>
-                                        <!-- Post Meta  -->
-                                        <div class="post-meta-wrapper with-button">
-                                            <div class="post-meta">
-                                                <div class="post-author-avatar border-rounded">
-                                                    <img src="assets/images/post-images/author/author-image-3.png"
-                                                         alt="Author Images">
-                                                </div>
-                                                <div class="content">
-                                                    <h6 class="post-author-name">
-                                                        <a class="hover-flip-item-wrapper" href="">
-                                                            <span class="hover-flip-item">
-                                                                <span data-text="Gamindu Hemachandra">Gamindu
-                                                                    Hemachandra</span>
-                                                            </span>
-                                                        </a>
-                                                    </h6>
-                                                    <ul class="post-meta-list">
-                                                        <li>Feb 17, 2019</li>
-                                                        <li>3 min read</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <ul class="social-share-transparent justify-content-end">
-                                                <li><a href="https://www.facebook.com/APIITofficial?mibextid=kFxxJD"><i
-                                                            class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="https://www.instagram.com/apiitsl?igsh=cjI0aHczMmthaDR2"><i
-                                                            class="fab fa-instagram"></i></a></li>
-                                                <li><a href="https://x.com/APIITsl?s=20"><i class="fab fa-twitter"></i></a>
-                                                </li>
-                                                <li><a href="https://youtube.com/@APIITedu?si=asVIXIdV5i59rdDF"><i
-                                                            class="fab fa-youtube"></i></a></li>
-                                                <li><a href="https://www.linkedin.com/company/apiit-sri-lanka/"><i
-                                                            class="fab fa-linkedin-in"></i></a></li>
-                                            </ul>
-                                            <div class="read-more-button cerchio">
-                                                <a class="axil-button button-rounded hover-flip-item-wrapper"
-                                                   href="">
-                                                    <span class="hover-flip-item">
-                                                        <span data-text="Read Post">Read Post</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- End Post Content  -->
-                                </div>
-                                <!-- End Single Slide  -->
-
-                                <!-- Start Single Slide  -->
-                                <div class="content-block">
-                                    <!-- Start Post Thumbnail  -->
-                                    <div class="post-thumbnail">
-                                        <a href="">
-                                            <img src="assets/images/post-images/gallery-post-02.jpg"
-                                                 alt="Post Images">
-                                        </a>
-                                    </div>
-                                    <!-- End Post Thumbnail  -->
-                                    <!-- Start Post Content  -->
-                                    <div class="post-content">
-                                        <div class="post-cat">
-                                            <div class="post-cat-list">
-                                                <a class="hover-flip-item-wrapper" href="#">
-                                                    <span class="hover-flip-item">
-                                                        <span
-                                                            data-text="Unveiling the Staffordshire University LLB">Unveiling
-                                                            the Staffordshire University LLB</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <h2 class="title"><a href="">A Dive into
-                                                Comprehensive Legal Knowledge</a></h2>
-
-                                        <!-- Post Meta  -->
-                                        <div class="post-meta-wrapper with-button">
-                                            <div class="post-meta">
-                                                <div class="post-author-avatar border-rounded">
-                                                    <img src="assets/images/post-images/author/author-image-2.png"
-                                                         alt="Author Images">
-                                                </div>
-                                                <div class="content">
-                                                    <h6 class="post-author-name">
-                                                        <a class="hover-flip-item-wrapper" href="">
-                                                            <span class="hover-flip-item">
-                                                                <span data-text="Dr Chathura Warnasuriya">Dr Chathura
-                                                                    Warnasuriya</span>
-                                                            </span>
-                                                        </a>
-                                                    </h6>
-                                                    <ul class="post-meta-list">
-                                                        <li>May 10, 2022</li>
-                                                        <li>4 min read</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <ul class="social-share-transparent justify-content-end">
-                                                <li><a href="https://www.facebook.com/APIITofficial?mibextid=kFxxJD"><i
-                                                            class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="https://www.instagram.com/apiitsl?igsh=cjI0aHczMmthaDR2"><i
-                                                            class="fab fa-instagram"></i></a></li>
-                                                <li><a href="https://x.com/APIITsl?s=20"><i class="fab fa-twitter"></i></a>
-                                                </li>
-                                                <li><a href="https://youtube.com/@APIITedu?si=asVIXIdV5i59rdDF"><i
-                                                            class="fab fa-youtube"></i></a></li>
-                                                <li><a href="https://www.linkedin.com/company/apiit-sri-lanka/"><i
-                                                            class="fab fa-linkedin-in"></i></a></li>
-                                            </ul>
-                                            <div class="read-more-button cerchio">
-                                                <a class="axil-button button-rounded hover-flip-item-wrapper"
-                                                   href="">
-                                                    <span class="hover-flip-item">
-                                                        <span data-text="Read Post">Read Post</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- End Post Content  -->
-                                </div>
-                                <!-- End Single Slide  -->
 
                                 {{--                            FEATURED POSTS DISPLAY SLIDER --}}
 
@@ -259,14 +116,14 @@
                                                     </div>
                                                     <ul class="social-share-transparent justify-content-end">
                                                         <li>
-                                                            <a href="https://www.facebook.com/APIITofficial?mibextid=kFxxJD"><i
+                                                            <a href="https://www.facebook.com/APIITofficial"><i
                                                                     class="fab fa-facebook-f"></i></a></li>
                                                         <li>
-                                                            <a href="https://www.instagram.com/apiitsl?igsh=cjI0aHczMmthaDR2"><i
+                                                            <a href="https://www.instagram.com/apiitsl"><i
                                                                     class="fab fa-instagram"></i></a></li>
                                                         <li><a href="https://x.com/APIITsl?s=20"><i
                                                                     class="fab fa-twitter"></i></a></li>
-                                                        <li><a href="https://youtube.com/@APIITedu?si=asVIXIdV5i59rdDF"><i
+                                                        <li><a href="https://youtube.com/@APIITedu"><i
                                                                     class="fab fa-youtube"></i></a></li>
                                                         <li><a href="https://www.linkedin.com/company/apiit-sri-lanka/"><i
                                                                     class="fab fa-linkedin-in"></i></a></li>
