b64c521396473b8f4eff9144a011a354ba4d59f6
Viduranga Lakshan <>
2024-05-19 15:40:51 +0530

    Update JobMiddleware.php

--- app/Http/Middleware/JobMiddleware.php
+++ app/Http/Middleware/JobMiddleware.php
@@ -10,7 +10,7 @@ class JobMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() && (Auth::user()->role->value === 1 || Auth::user()->role->value === 4 || (Auth::user()->role->value === 7 && Auth::user()->degree_level === 6) || (Auth::user()->role->value === 7 && Auth::user()->degree_level === 7) || (Auth::user()->role->value === 7 && Auth::user()->degree_level === 8) || (Auth::user()->role->value === 7 && Auth::user()->degree_level === 9) || (Auth::user()->role->value === 7 && Auth::user()->degree_level === 10))) {
+        if (Auth::user() && (Auth::user()->role->value == 1 || Auth::user()->role->value == 4 || (Auth::user()->role->value == 7 && Auth::user()->degree_level == 6) || (Auth::user()->role->value == 7 && Auth::user()->degree_level == 7) || (Auth::user()->role->value == 7 && Auth::user()->degree_level == 8) || (Auth::user()->role->value == 7 && Auth::user()->degree_level == 9) || (Auth::user()->role->value == 7 && Auth::user()->degree_level == 10))) {
             return $next($request);
         }
         abort(404);
