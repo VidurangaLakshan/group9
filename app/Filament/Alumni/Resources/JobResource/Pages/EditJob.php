fb68cd5607c019e333aa804d0d8e1fa7587ea863
Viduranga Lakshan <>
2024-05-21 09:16:00 +0530

    edit job notification

--- app/Filament/Alumni/Resources/JobResource/Pages/EditJob.php
+++ app/Filament/Alumni/Resources/JobResource/Pages/EditJob.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Alumni\Resources\JobResource\Pages;
 
 use App\Filament\Alumni\Resources\JobResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditJob extends EditRecord
@@ -15,5 +17,16 @@ class EditJob extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function afterSave() : void
+    {
+        $job = $this->record;
+
+        Notification::make()
+            ->title('Job needs approval')
+            ->body("'{$job->title}' has been created but is not visible.")
+            ->sendToDatabase(User::where('role', 4)->get());
+
     }
 }
